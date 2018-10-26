a46f7ac92ea2c7d22ff84f6e8ba106423689e133
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-10-26 09:31:37 +0200

    Update Rossbob.c

--- Rossbob.c
+++ Rossbob.c
@@ -1,1 +1,15 @@
+/* Include le ficier header (*.h) */
+#include "rossbob.h"
 
+/* Le fonction defini dans le header */
+int feedcat (int foodstate, int rbappear)
+{
+	if (foodstate < FOODREQ/4 && rbappear = 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
