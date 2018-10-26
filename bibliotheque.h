034720368a9b5ab392a06fd40bce9dbf15cbc323
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-10-26 10:46:28 +0200

    Update bibliotheque.h

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,7 +1,15 @@
 #ifndef BIBLIOTHEQUE_H
 #define BIBLIOTHEQUE_H
 
-
+/* les macro pour le francais fainéant */
+#define DEBUT {
+#define FIN }
+#define NUMERO int
+#define TANT_QUE while
+#define SI if
+#define POUR for
+#define IMPRIM printf
+#define SCAN scanf
 
 
 
