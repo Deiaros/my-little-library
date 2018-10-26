e875f6b1a32c766b3f4217139de652293938d12e
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-10-26 10:04:03 +0200

    Create NCBLS.C

---
+++ NCBLS.C
@@ -0,0 +1,13 @@
+#include NCDLS.H
+
+int noob(int synergy, int tricolor)
+{
+  if (noob<NCBLS+synergy+tricolor)
+  printf ("\n ne joue pas Nicol Bolas")
+    return 1
+  }
+  else
+  {
+  return 0
+  }
+}
