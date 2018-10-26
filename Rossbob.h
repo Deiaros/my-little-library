42e7efe68bed7e0483f0f015f6280648d68a31e0
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-10-26 09:31:59 +0200

    Update Rossbob.h

--- Rossbob.h
+++ Rossbob.h
@@ -1,1 +1,10 @@
+/* Definir et commence la bibliotheque */
+#ifndef ROSSBOB_H //commencement de la bibliotheque
+#define ROSSBOB_H //definir la bibliotheque
 
+#define FOODREQ 30 //une macro pour "foodreq"
+
+/* La fonction "feedcat" et defini */
+int feedcat (int foodstate, int rbappear) 
+
+#endif //fin de la bibliotheque
