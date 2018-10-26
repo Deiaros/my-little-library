50ba23dd8d0a5ce5d3a2a53d4c9f2b17159aaf52
LilWaTcHuRs <44434793+LilWaTcHuRs@users.noreply.github.com>
2018-10-26 14:04:28 +0200

    Create math.h

---
+++ math.h
@@ -0,0 +1,8 @@
+#define addition(x, y) ((x) + (y))
+#define soustraction(x, y) ((x) - (y))
+#define multiplication(x, y) ((x) * (y))
+#define division(x, y) ((x) / (y) && (y) != 0)
+#define comp(x, y) ((x) == (y))
+#define sqr(x) ((x) * (x))
+
+int division(int x, int y) 
